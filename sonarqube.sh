3dc027ffe00d2940cb9eed93c332d6c43c4731bd
senestro9729 <senestro9729@gmail.com>
2022-10-26 19:13:03 -0500

    sonar

---
+++ sonarqube.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##author: ibrahim
+##date: 25 oct 2022
+##sonarqube installation on centos 7
+##please run this as vagrant
+
+user_name=`whoami`
+
+
+if [ $user_name != vagrant ];
+
+then
+
+echo "Must be run as user vagrant !!!!"
+exit 1
+fi
+
+sudo yum update -y
+sudo yum install java-11-openjdk-devel -y
+sudo yum install java-11-openjdk -y
+cd /opt
+sudo yum install wget unzip -y
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
+sudo unzip /opt/sonarqube-9.3.0.51899.zip
+sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
+cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64/
+sudo firewall-cmd --permanent --add-port=9000/tcp
+sudo firewall-cmd --reload
+ ./sonar.sh start
+echo "Installation successfully completed please get the ip and access your sonarqube on the browser"
